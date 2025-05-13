480ec31f5adcf24cf3270b86b7bac227198465bb
Giovanna Matos <giovannammatos07@gmail.com>
2025-05-13 19:13:07 -0300

    Update calculadora.py

--- Desafio1/calculadora.py
+++ Desafio1/calculadora.py
@@ -9,29 +9,31 @@ def repetir():
         repetir()
 
 def calculadora():
-    operador = input("Qual operação deseja realizar? (+, -, *, /)")
-    num1 = int(input('Digite o primeiro número: '))
-    num2 = int(input('Digite o segundo número: '))
+    try:
+        num1 = int(input('Digite o primeiro número: '))
+        num2 = int(input('Digite o segundo número: '))
+        operador = input("Qual operação deseja realizar? (+, -, *, /)")
+        
+        if operador == "+":
+            print(num1+num2)
 
-    if operador == "+":
-        print(num1+num2)
+        elif operador == "-":
+            print(num1-num2)
 
-    elif operador == "-":
-        print(num1-num2)
+        elif operador == "*":
+            print(num1*num2)
 
-    elif operador == "*":
-        print(num1*num2)
+        elif operador == "/":
+            if num2 == 0:
+                print("Não é possivel dividir por 0")
+                repetir()
+            else:
+                print(num1/num2)
 
-    elif operador == "/":
-        if num2 == 0:
-            print("Não é possivel dividir por 0")
-            repetir()
         else:
-            print(num1/num2)
-
-    else:
-        print('Insira um operador válido')
-    
+            print('Insira um operador válido')
+    except ValueError:
+        print("Digite um número válido")
     repetir()
 
 calculadora()
